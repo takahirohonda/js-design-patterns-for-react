6d07326289a45477174d627006a21a1bdbc495fd
takahirohonda <thon6thon6@hotmail.com>
2025-02-05 17:09:54 +1100

    crystal ball in progress...

---
+++ apps/node-app/src/crystalBall.ts
@@ -0,0 +1,6 @@
+// Determine the highest point where you can drop a crystal ball without breaking it in the input array.
+// Input is an array of height [1, 10, 100, 120, 200]. The breaking point is 130. You only have 2 crystal balls.
+// Therefore the function should return 120.
+export const crystalBall = (heightArray: number[], breakingPoint: number) => {
+  const middle = heightArray[heightArray.length / 2]
+}
