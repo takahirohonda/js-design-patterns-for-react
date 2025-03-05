193c24ca7de83fe2182cb7dcac3233b06f6ff750
takahirohonda <thon6thon6@hotmail.com>
2025-03-05 10:44:43 +0900

    Add js array performance test

--- apps/node-app/src/arrayBuffer-ringBuffer/jsArrayPerformance.spec.ts
+++ apps/node-app/src/arrayBuffer-ringBuffer/jsArrayPerformance.spec.ts
@@ -1,11 +1,54 @@
-import { getExecutionDuration, unshift } from './jsArrayPerformance'
+import {
+  getExecutionDuration,
+  pop,
+  push,
+  shift,
+  unshift,
+} from './jsArrayPerformance'
 
 describe('Empirically deducing JS array algorithm', () => {
+  const unshiftPerformance = []
+  const shiftPerformance = []
+  const pushPerformance = []
+  const popPerformance = []
+
+  afterEach(() => {
+    console.log('Performance for unshift operation')
+    console.log(JSON.stringify(unshiftPerformance))
+    console.log(`shift performance: ${JSON.stringify(shiftPerformance)}`)
+    console.log(`push performance: ${JSON.stringify(pushPerformance)}`)
+    console.log(`pop performance: ${JSON.stringify(popPerformance)}`)
+  })
+
   it('test', () => {
     const sizeArray = [10, 100, 1000, 10000, 100000]
+    // const sizeArray2 = [10, 100, 1000, 10000, 100000, 1_000_000, 10_000_000]
+    sizeArray.forEach((size) => {
+      unshiftPerformance.push({
+        size: size,
+        time: getExecutionDuration(() => unshift(size)),
+      })
+    })
 
     sizeArray.forEach((size) => {
-      console.log(getExecutionDuration(() => unshift(size)))
+      shiftPerformance.push({
+        size: size,
+        time: getExecutionDuration(() => shift(size)),
+      })
+    })
+
+    sizeArray.forEach((size) => {
+      pushPerformance.push({
+        size: size,
+        time: getExecutionDuration(() => push(size)),
+      })
+    })
+
+    sizeArray.forEach((size) => {
+      popPerformance.push({
+        size: size,
+        time: getExecutionDuration(() => pop(size)),
+      })
     })
 
     expect(1 + 1).toBe(2)
--- apps/node-app/src/arrayBuffer-ringBuffer/jsArrayPerformance.ts
+++ apps/node-app/src/arrayBuffer-ringBuffer/jsArrayPerformance.ts
@@ -8,6 +8,32 @@ const a: number[] = []
 
 export const unshift = (size: number) => {
   for (let i = 0; i < size; ++i) {
+    // unshift adds the argument value to the beginning,
+    // and returns a new length of the array
     a.unshift(Math.random())
   }
+}
+
+export const shift = (size: number) => {
+  for (let i = 0; i < size; ++i) {
+    // shift removes the first element from an array
+    // and returns a new array
+    a.shift()
+  }
+}
+
+export const push = (size: number) => {
+  for (let i = 0; i < size; ++i) {
+    a.push(Math.random())
+  }
+}
+
+export const pop = (size: number) => {
+  for (let i = 0; i < size; ++i) {
+    a.pop()
+  }
+}
+
+export const get = (idx: number) => {
+  return a[idx]
 }
