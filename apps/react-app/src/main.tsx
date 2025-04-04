ea3db1bd44cecf3d7c18d25ce168ad9ed7a39bb2
takahirohonda <thon6thon6@hotmail.com>
2025-04-04 12:38:51 +1100

    feat: add routes to react

--- apps/react-app/src/main.tsx
+++ apps/react-app/src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { BrowserRouter } from 'react-router'
 import * as ReactDOM from 'react-dom/client'
-import { App } from './app/app'
+import { AppRoutes } from './AppRoutes'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppRoutes />
     </BrowserRouter>
   </StrictMode>
 )
