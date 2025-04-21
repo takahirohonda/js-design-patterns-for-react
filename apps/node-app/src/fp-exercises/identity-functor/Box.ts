596c222eae0738a819ad6f76491578162a2ca1a8
takahirohonda <thon6thon6@hotmail.com>
2025-04-22 09:47:54 +1000

    more updates

--- apps/node-app/src/fp-exercises/identity-functor/Box.ts
+++ apps/node-app/src/fp-exercises/identity-functor/Box.ts
@@ -1,3 +1,4 @@
+// Box is a functor https://en.wikipedia.org/wiki/Functor_(functional_programming)
 export const Box = (x) => ({
   map: (f) => Box(f(x)),
   fold: (f) => f(x), // just passing the value to the function
