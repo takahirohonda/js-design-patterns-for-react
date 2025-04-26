869e40367bf78b63a01097f16490e050eeea8668
takahirohonda <thon6thon6@hotmail.com>
2025-04-27 09:26:23 +1000

    either monad

--- apps/node-app/src/fp-exercises/either-monad/either-monad-exercises-1.spec.ts
+++ apps/node-app/src/fp-exercises/either-monad/either-monad-exercises-1.spec.ts
@@ -1,7 +1,7 @@
-import { fromNullable } from './left-right'
+import { fromNullable, tryCatch } from './left-right'
 import fs from 'fs'
 
-describe('Either Monad Exercise', () => {
+describe('Either Monad Exercise 1', () => {
   describe('Ex 1', () => {
     const data = { red: '#ff4444', blue: '#3b5998', yellow: '#fff68f' }
     const findColor = (name) => data[name].toUpperCase()
@@ -14,10 +14,12 @@ describe('Either Monad Exercise', () => {
 
     it('using either monad', () => {
       const format = (x) =>
-        fromNullable(data[x]).fold(
-          () => 'unexpected_error',
-          (x) => x.toUpperCase()
-        )
+        fromNullable(data[x])
+          .map((x) => x.toUpperCase())
+          .fold(
+            () => 'unexpected_error',
+            (x) => x
+          )
       expect(format('red')).toBe('#FF4444')
       expect(format('redd')).toBe('unexpected_error')
     })
@@ -43,15 +45,11 @@ describe('Either Monad Exercise', () => {
     })
 
     it('should return port with either monad', () => {
-      const getStringFromConfigFile = (filePath) => {
-        try {
-          return fs.readFileSync(filePath, 'utf-8')
-        } catch {
-          return null
-        }
-      }
+      const readFileSyncFn = (filePath) =>
+        tryCatch(() => fs.readFileSync(filePath, 'utf-8'))
+
       const getPort = (filePath) =>
-        fromNullable(getStringFromConfigFile(filePath))
+        readFileSyncFn(filePath)
           .map((x) => JSON.parse(x))
           .fold(
             () => 3000,
@@ -59,8 +57,21 @@ describe('Either Monad Exercise', () => {
           )
 
       expect(getPort(FILE_PATH)).toBe(8000)
-
       expect(getPort('./non-existent-file.json')).toBe(3000)
+
+      // handle JSON.parse error, as well
+      const parseJSONFn = (json) => tryCatch(() => JSON.parse(json))
+
+      const getPort2 = (filePath) =>
+        readFileSyncFn(filePath)
+          .chain((x) => parseJSONFn(x))
+          .map((x) => x.port)
+          .fold(
+            () => 3000,
+            (x) => x
+          )
+      expect(getPort2(FILE_PATH)).toBe(8000)
+      expect(getPort2('./non-existent-file.json')).toBe(3000)
     })
   })
 })
--- apps/node-app/src/fp-exercises/either-monad/left-right.ts
+++ apps/node-app/src/fp-exercises/either-monad/left-right.ts
@@ -1,21 +1,23 @@
 const Right = (x) => ({
+  chain: (f) => f(x),
   map: (f) => Right(f(x)),
   fold: (f, g) => g(x),
   inspect: () => `Right(${x})`,
 })
 
 const Left = (x) => ({
-  map: (f) => Left(f(x)),
+  chain: (f) => Left(x),
+  map: (f) => Left(x),
   fold: (f, g) => f(x),
   inspect: () => `Left(${x})`,
 })
 
-export const fromNullable = (x) => (x != null ? Right(x) : Left(null))
+export const fromNullable = (x) => (x ? Right(x) : Left(null))
 
 export const tryCatch = (f) => {
   try {
     return Right(f())
   } catch (e) {
-    Left(e)
+    return Left(e)
   }
 }
