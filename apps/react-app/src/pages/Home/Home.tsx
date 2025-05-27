b07151bb948038b4a91ce5a9fa4bbcb36248d11b
takahirohonda <thon6thon6@hotmail.com>
2025-05-27 21:15:03 +1000

    fix: update

--- apps/react-app/src/pages/Home/Home.tsx
+++ apps/react-app/src/pages/Home/Home.tsx
@@ -1,3 +1,48 @@
+import { useEffect, useState } from 'react'
+
 export const Home = () => {
-  return <div>Place holder for Home</div>
+  const [driverData, setDriverData] = useState([])
+  useEffect(() => {
+    fetch('https://api.openf1.org/v1/drivers?session_key=9979').then((data) => {
+      data.json().then((data) => {
+        setDriverData(data)
+      })
+    })
+  }, [])
+  return (
+    <div className="h-full bg-gray-900 text-gray-100">
+      <div className="container mx-auto py-[40px]">
+        <table className="table-auto border-collapse border border-gray-700 w-full">
+          <thead>
+            <tr>
+              <th className="border border-gray-700 px-4 py-2">Headshot</th>
+              <th className="border border-gray-700 px-4 py-2">Full Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {driverData.map((driver: any) => (
+              <tr
+                key={
+                  driver.driver_number ||
+                  `${driver.first_name}-${driver.last_name}`
+                }
+                className="hover:bg-gray-800"
+              >
+                <td className="border border-gray-700 px-4 py-2">
+                  <img
+                    src={driver.headshot_url}
+                    alt={`${driver.first_name} ${driver.last_name}`}
+                    className="w-16 h-16 object-cover rounded"
+                  />
+                </td>
+                <td className="border border-gray-700 px-4 py-2">
+                  {`${driver.first_name} ${driver.last_name}`}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
