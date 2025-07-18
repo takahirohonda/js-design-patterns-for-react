311ab6fd212a77aec41a1345a422838302386502
takahirohonda <thon6thon6@hotmail.com>
2025-07-18 16:16:39 +1000

    Add unit tests

--- libs/utils-sub-pub/src/lib/utils-sub-pub.spec.tsx
+++ libs/utils-sub-pub/src/lib/utils-sub-pub.spec.tsx
@@ -1,10 +1,55 @@
-import { render } from '@testing-library/react'
+import {
+  publish,
+  subscribe,
+  unsubscribe,
+  unsubscribeAll,
+} from './utils-sub-pub'
 
-import UtilsSubPub from './utils-sub-pub'
+describe('utils-sub-pub', () => {
+  beforeEach(() => {
+    // Clean up all listeners before each test
+    unsubscribeAll('test-event')
+    unsubscribeAll('another-event')
+  })
 
-describe('UtilsSubPub', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<UtilsSubPub />)
-    expect(baseElement).toBeTruthy()
+  it('should call subscribed listener when event is published', () => {
+    const listener = jest.fn()
+    subscribe('test-event', listener)
+    publish('test-event', 'hello')
+    expect(listener).toHaveBeenCalledWith('hello')
+  })
+
+  it('should not call unsubscribed listener', () => {
+    const listener = jest.fn()
+    subscribe('test-event', listener)
+    unsubscribe('test-event', listener)
+    publish('test-event', 'data')
+    expect(listener).not.toHaveBeenCalled()
+  })
+
+  it('should not call listener for different event', () => {
+    const listener = jest.fn()
+    subscribe('test-event', listener)
+    publish('another-event', 'data')
+    expect(listener).not.toHaveBeenCalled()
+  })
+
+  it('should not subscribe the same listener twice', () => {
+    const listener = jest.fn()
+    subscribe('test-event', listener)
+    subscribe('test-event', listener)
+    publish('test-event', 'foo')
+    expect(listener).toHaveBeenCalledTimes(1)
+  })
+
+  it('should remove all listeners for an event with unsubscribeAll', () => {
+    const listener1 = jest.fn()
+    const listener2 = jest.fn()
+    subscribe('test-event', listener1)
+    subscribe('test-event', listener2)
+    unsubscribeAll('test-event')
+    publish('test-event', 'bar')
+    expect(listener1).not.toHaveBeenCalled()
+    expect(listener2).not.toHaveBeenCalled()
   })
 })
--- libs/utils-sub-pub/src/lib/utils-sub-pub.tsx
+++ libs/utils-sub-pub/src/lib/utils-sub-pub.tsx
@@ -1,9 +1,42 @@
-export function UtilsSubPub() {
-  return (
-    <div className={styles['container']}>
-      <h1>Welcome to UtilsSubPub!</h1>
-    </div>
-  )
+const listeners: Record<string, ((data: unknown) => void)[]> = {}
+
+export const publish = (eventName: string, data: unknown) => {
+  const listenerArray = listeners[eventName]
+  if (listenerArray && listenerArray.length > 0) {
+    listenerArray.forEach((listener) => {
+      listener(data)
+    })
+  }
 }
 
-export default UtilsSubPub
+export const subscribe = (eventName: string, listener: any) => {
+  const listenerForEvent = listeners[eventName]
+  if (!listenerForEvent) {
+    listeners[eventName] = [listener]
+    return
+  }
+  const hasAlreadySubscribed =
+    listenerForEvent.filter((existingListener) => {
+      return existingListener === listener
+    }).length > 0
+  if (hasAlreadySubscribed) {
+    return
+  }
+  listenerForEvent.push(listener)
+}
+
+export const unsubscribe = (eventName: string, listener: any) => {
+  const listenerForEvent = listeners[eventName]
+
+  if (!listenerForEvent) {
+    return
+  }
+
+  listeners[eventName] = listenerForEvent.filter((existingListener) => {
+    return existingListener !== listener
+  })
+}
+
+export const unsubscribeAll = (eventName: string) => {
+  delete listeners[eventName]
+}
