88b8751a9c8f91c0ab6fe4f35efea96f92b11852
takahirohonda <thon6thon6@hotmail.com>
2025-07-18 16:34:03 +1000

    update

--- random-code/practise2.ts
+++ random-code/practise2.ts
@@ -1,31 +1,3 @@
-const listeners: ((data: unknown) => void)[] = []
-
-export const subscribe = (listener: (data: unknown) => void): void => {
-  const hasAlreadySubscribed = listeners.includes(listener)
-  if (hasAlreadySubscribed) {
-    return
-  }
-  listeners.push(listener)
-}
-
-export const unsubscribe = (listener: (data: unknown) => void): void => {
-  const updatedListeners = listeners.filter((l) => l !== listener)
-  listeners.length = 0
-  listeners.push(...updatedListeners)
-}
-
-export const notify = (data: unknown): void => {
-  listeners.forEach((listener) => listener(data))
-}
-
-// fibonacci function
-export const fibonacci = (n: number): number => {
-  if (n <= 1) {
-    return n
-  }
-  return fibonacci(n - 1) + fibonacci(n - 2)
-}
-
 // linked list
 export interface LinkedListNode<T> {
   value: T
